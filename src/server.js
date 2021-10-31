cb287f167766aff0a20c7ff02657972d7117a790
j0tac3 <j0s3.90jr@gmail.com>
2021-10-31 15:28:38 +0100

    Despliegue de la app

--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
-
-const path = require('path');
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/mel-sb'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/mel-sb/index.html'));
+app.use(express.static(__dirname+'/dist/mel-sb'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/mel-sb/index.html'));
 });
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
