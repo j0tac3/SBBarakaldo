dcbba6e69dfd3ddf9730e8e1134797cbdeefaa40
j0tac3 <j0s3.90jr@gmail.com>
2021-11-07 17:56:39 +0100

    Actualizacion de repositirios y cambiosd e diseño

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mel-sb'));
+app.use(express.static(__dirname + '/dist/MelSB'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/mel-sb/index.html'));
+res.sendFile(path.join(__dirname+'/dist/MelSB/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
